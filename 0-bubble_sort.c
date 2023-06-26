80a0d1f43a4cac402a4ec04321fd2cfd5783c126
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-06-26 11:28:17 -0700

    ggg

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,17 +6,23 @@
  * @array: the array to be sorted
  * @size: number of elements in @array
  */
-void bubble_sort(int *array, size_t size) {
+#include <stdio.h>
+
+void bubble_sort(int *array, size_t size)
+{
     int temp;
-    for (size_t i = 0; i < size - 1; i++) {
-        for (size_t j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j+1]) {
+    for (size_t i = 0; i < size - 1; i++)
+    {
+        for (size_t j = 0; j < size - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
                 temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
             }
-            // Print array after each swap
-            for (size_t k = 0; k < size; k++) {
+            for (size_t k = 0; k < size; k++)
+            {
                 printf("%d ", array[k]);
             }
             printf("\n");
