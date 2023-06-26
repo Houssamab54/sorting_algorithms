510ce0ce56ac26bc6aa7a29ee3a9d4363042d52a
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-06-26 11:29:32 -0700

    ggg

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,10 +10,10 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    int temp;
-    for (size_t i = 0; i < size - 1; i++)
+  int temp, i, j, k;
+    for (i = 0; i < size - 1; i++)
     {
-        for (size_t j = 0; j < size - i - 1; j++)
+        for (j = 0; j < size - i - 1; j++)
         {
             if (array[j] > array[j + 1])
             {
@@ -21,7 +21,7 @@ void bubble_sort(int *array, size_t size)
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
             }
-            for (size_t k = 0; k < size; k++)
+            for (k = 0; k < size; k++)
             {
                 printf("%d ", array[k]);
             }
